bfd5ec295f5caf5ea960b79a2a16b6f4fd171785
jaycole <jdotcole233@gmail.com>
2022-03-15 09:26:59 +0000

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,19 @@
  *
  * Description: Output _putchar to screen
  *
- * return: Always return 0 (Success)
+ * Return: Always return 0 (Success)
  */
 
 int main(void)
 {
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while (c[i]!= '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
